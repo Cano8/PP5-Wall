df64a286c6c43d0676528cc71ae17833a187e212
Cano8 <tolto8@gmail.com>
2015-04-23 19:15:08 +0200

    cleanup

--- README.md
+++
@@ -1,3 +0,0 @@
-PP5-Wall
-
-A simple project for Programming Workshop classes
--- web/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-error_reporting(-1);
-ini_set('error_reporting', E_ALL);
-
-include_once(__DIR__.'/../app/app.php');
